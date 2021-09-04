9fea98c7bf9f5ebbb273570e3369a8f07246ff39
Casa <carlos_henrique.taqua@hotmail.com>
2021-09-03 23:18:25 -0300

    Criando compartilhamento do cartão de visitas com outros apps

--- app/src/main/java/com/chs/businesscard/ui/MainActivity.kt
+++ app/src/main/java/com/chs/businesscard/ui/MainActivity.kt
@@ -1,41 +1,61 @@
 package com.chs.businesscard.ui
 
-import android.app.Activity
+import android.Manifest
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.activity.viewModels
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
 import com.chs.businesscard.App
-import com.chs.businesscard.R
 import com.chs.businesscard.databinding.ActivityMainBinding
+import com.chs.businesscard.util.Image
 
 class MainActivity : AppCompatActivity() {
-    private val binding by lazy {ActivityMainBinding.inflate(layoutInflater)}
-    private val mainViewModel: MainViewModel by viewModels{
+
+    private val binding by lazy { ActivityMainBinding.inflate(layoutInflater) }
+    private val mainViewModel: MainViewModel by viewModels {
         MainViewModelFactory((application as App).repository)
     }
-
     private val adapter by lazy { BusinesscardAdapter() }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
+        setUpPermissions()
         binding.rvCard.adapter = adapter
         getAllBusinessCard()
-        insertListener()
+        insertListeners()
     }
 
-    private fun insertListener(){
-        binding.fab.setOnClickListener{
-            val intent = Intent(this@MainActivity, AddBusinessCardActivity::class.java)
+    private fun setUpPermissions() {
+        // write permission to access the storage
+        ActivityCompat.requestPermissions(
+            this,
+            arrayOf(Manifest.permission.WRITE_EXTERNAL_STORAGE),
+            1
+        )
+        ActivityCompat.requestPermissions(
+            this,
+            arrayOf(Manifest.permission.READ_EXTERNAL_STORAGE),
+            1
+        )
+    }
+
+    private fun insertListeners() {
+        binding.fab.setOnClickListener {
+            val intent = Intent(this, AddBusinessCardActivity::class.java)
             startActivity(intent)
         }
-    }
-
-    private fun getAllBusinessCard(){
-        mainViewModel.getAll().observe(this){businessCard ->
-            adapter.submitList(businessCard)
+        adapter.listenerShare = { card ->
+            Image.share(this@MainActivity, card)
         }
     }
+
+    private fun getAllBusinessCard() {
+        mainViewModel.getAll().observe(this, { businessCards ->
+            adapter.submitList(businessCards)
+        })
+    }
+
 
 }
